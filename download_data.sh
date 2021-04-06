bd3735b571696095b62a8927fbcc72acba0f28c3
ArjunBhalla98 <arjunbhallasg@gmail.com>
2021-04-06 19:53:06 -0400

    update shell script

--- download_data.sh
+++ download_data.sh
@@ -6,33 +6,22 @@ else
     total_size="$(($num_categories * 2))"
 
     while true; do
-        case $yn in
-            [Yy]* ) 
-                for category in "$@"
-                do
-                    for data in "test" "val" "train"
-                    do
-                        ## eg, airplane_test
-                        filename=${category}_${data}
-                        ## download tar from csail server
-                        wget rin.csail.mit.edu/data/${filename}.tar.gz; 
-                        echo "Extracting to "dataset/output/${filename}"..."
-                        ## extract tar
-                        tar xf ${filename}.tar.gz 
-                        ## move to data folder
-                        mv ${filename} dataset/output/${filename}
-                        ## remove tar
-                        rm ${filename}.tar.gz
-                    done
-                done
-                break;;
-            
-            [Nn]* ) 
-                exit;;
-            
-            * ) 
-                echo "Please answer yes or no.";;
-
-        esac
+	for category in "$@"
+	do
+	    for data in "test" "val" "train"
+	    do
+		## eg, airplane_test
+		filename=${category}_${data}
+		## download tar from csail server
+		wget rin.csail.mit.edu/data/${filename}.tar.gz; 
+		echo "Extracting to "dataset/output/${filename}"..."
+		## extract tar
+		tar xf ${filename}.tar.gz 
+		## move to data folder
+		mv ${filename} dataset/output/${filename}
+		## remove tar
+		rm ${filename}.tar.gz
+	    done
+	done
     done
 fi
