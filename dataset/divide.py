7a5ff9d8dbc2cee12db4bf37fc87a1d3df109eb4
ArjunBhalla98 <arjunbhallasg@gmail.com>
2021-04-08 12:24:42 -0400

    removed python 2.7 prints completely

--- dataset/divide.py
+++ dataset/divide.py
@@ -1,16 +1,35 @@
 import os, argparse, subprocess
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--low',        default=0,                      type=int,   help='min image index')
-parser.add_argument('--high',       default=200,                  type=int,   help='max image index')
-parser.add_argument('--repeat',     default=10,                      type=int,   help='number of renderings per object')
-parser.add_argument('--division',   default=10,                     type=int,   help='number of images per SLURM job')
-parser.add_argument('--qos',        default=False,                  type=bool,  help='whether to use quality-of-service queue')
-parser.add_argument('--jobs',       default=None,                               help='list of (low, high) indices to render, overrides {low,high,repeat,division}')
-parser.add_argument('--script',     default='run.py',               type=str,   help='script to launch blender job')
-parser.add_argument('--mode',       default='render.py',            type=str,   help='script run within blender')
-parser.add_argument('--category',   default='car',            type=str,   help='object category (from ShapeNet or primitive, see options in config.py)')
-parser.add_argument('--output',     default='output/car_val/',    type=str,   help='save folder')
+parser.add_argument("--low", default=0, type=int, help="min image index")
+parser.add_argument("--high", default=200, type=int, help="max image index")
+parser.add_argument(
+    "--repeat", default=10, type=int, help="number of renderings per object"
+)
+parser.add_argument(
+    "--division", default=10, type=int, help="number of images per SLURM job"
+)
+parser.add_argument(
+    "--qos", default=False, type=bool, help="whether to use quality-of-service queue"
+)
+parser.add_argument(
+    "--jobs",
+    default=None,
+    help="list of (low, high) indices to render, overrides {low,high,repeat,division}",
+)
+parser.add_argument(
+    "--script", default="run.py", type=str, help="script to launch blender job"
+)
+parser.add_argument(
+    "--mode", default="render.py", type=str, help="script run within blender"
+)
+parser.add_argument(
+    "--category",
+    default="car",
+    type=str,
+    help="object category (from ShapeNet or primitive, see options in config.py)",
+)
+parser.add_argument("--output", default="output/car_val/", type=str, help="save folder")
 args = parser.parse_args()
 
 
@@ -24,19 +43,40 @@ def use_divisions(args):
     for (start, end) in args.jobs:
         launch(args, start, end)
 
-def launch(args, start, end):
-    print 'Submitting: ', start, end
-        
-    working_dir = os.path.dirname(os.path.realpath(__file__))
-    repo_folder = os.path.join(working_dir, '..')
 
-    command = [   'sbatch', '-c', '1', \
-                            '-J', str(start)+'_'+str(end), '--time=1-12:0', \
-                            '--mem=5G', args.script, '--script', args.mode, '--low', str(start), '--high', str(end), \
-                            '--repeat', str(args.repeat), '--category', args.category, '--output', args.output, '--include', repo_folder]
+def launch(args, start, end):
+    print("Submitting: ", start, end)
+
+    working_dir = os.path.dirname(os.path.realpath(__file__))
+    repo_folder = os.path.join(working_dir, "..")
+
+    command = [
+        "sbatch",
+        "-c",
+        "1",
+        "-J",
+        str(start) + "_" + str(end),
+        "--time=1-12:0",
+        "--mem=5G",
+        args.script,
+        "--script",
+        args.mode,
+        "--low",
+        str(start),
+        "--high",
+        str(end),
+        "--repeat",
+        str(args.repeat),
+        "--category",
+        args.category,
+        "--output",
+        args.output,
+        "--include",
+        repo_folder,
+    ]
     if args.qos:
-        command.insert(1, '--qos=tenenbaum')
-    p = subprocess.call( command )
+        command.insert(1, "--qos=tenenbaum")
+    p = subprocess.call(command)
 
 
 if args.jobs:
--- dataset/run.py
+++ dataset/run.py
@@ -3,31 +3,65 @@
 import os, argparse, subprocess
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--low',        default=0,              type=int, help='min image index')
-parser.add_argument('--high',       default=100,            type=int, help='max image index')
-parser.add_argument('--repeat',     default=2,              type=int, help='number of renderings per object')
-parser.add_argument('--category',   default='car',          type=str, help='object category (from ShapeNet or primitive, see options in config.py)')
-parser.add_argument('--output',     default='output/car/',  type=str, help='save folder')
-parser.add_argument('--script',     default='render.py',    type=str, help='script run within blender')
-parser.add_argument('--include',    default=None,                     help='directory to include in python path')
+parser.add_argument("--low", default=0, type=int, help="min image index")
+parser.add_argument("--high", default=100, type=int, help="max image index")
+parser.add_argument(
+    "--repeat", default=2, type=int, help="number of renderings per object"
+)
+parser.add_argument(
+    "--category",
+    default="car",
+    type=str,
+    help="object category (from ShapeNet or primitive, see options in config.py)",
+)
+parser.add_argument("--output", default="output/car/", type=str, help="save folder")
+parser.add_argument(
+    "--script", default="render.py", type=str, help="script run within blender"
+)
+parser.add_argument(
+    "--include", default=None, help="directory to include in python path"
+)
 args = parser.parse_args()
+
 
 def render(script, low, high, repeat, category, output):
     if args.include is None:
         working_dir = os.path.dirname(os.path.realpath(__file__))
-        repo_folder = os.path.join(working_dir, '..')
+        repo_folder = os.path.join(working_dir, "..")
     else:
         repo_folder = args.include
 
-    command = ['/om/user/janner/blender-2.76b-linux-glibc211-x86_64/blender', '--background', '-noaudio', '--python', script, '--', '--include', repo_folder, \
-        '--start', low, '--finish', high, '--repeat', repeat, '--category', category, '--output', output] #, \
+    command = [
+        "/om/user/janner/blender-2.76b-linux-glibc211-x86_64/blender",
+        "--background",
+        "-noaudio",
+        "--python",
+        script,
+        "--",
+        "--include",
+        repo_folder,
+        "--start",
+        low,
+        "--finish",
+        high,
+        "--repeat",
+        repeat,
+        "--category",
+        category,
+        "--output",
+        output,
+    ]  # , \
 
     p = subprocess.call(command)
 
 
-if __name__ == '__main__':
-    print args
-    render(args.script, str(args.low), str(args.high), str(args.repeat), args.category, args.output)
-
-
-
+if __name__ == "__main__":
+    print(args)
+    render(
+        args.script,
+        str(args.low),
+        str(args.high),
+        str(args.repeat),
+        args.category,
+        args.output,
+    )
