4bce01ef8e3911bf3cda4b176f2fc1b4b8b59186
ArjunBhalla98 <arjunbhallasg@gmail.com>
2021-04-21 18:18:44 -0400

    composer update

--- models/Composer.py
+++ models/Composer.py
@@ -26,6 +26,58 @@ if __name__ == "__main__":
     sys.path.append("../")
     import models
 
+    from PIL import Image, ImageOps
+    import imageio
+    import torchvision.transforms
+
+    # img = Image.open("/phoenix/S3/ab2383/data/train_imgs/00277_0005.png")
+    # mask = Image.open("/phoenix/S3/ab2383/data/TikTok_dataset/00277/masks/0005.png")
+    img = Image.open(
+        "/home/ab2383/intrinsics-network/dataset/output/motorbike_test/2_composite.png"
+    )
+    mask = Image.open(
+        "/home/ab2383/intrinsics-network/dataset/output/motorbike_test/2_mask.png"
+    )
+    w, h = img.size  # why is this wxh??
+    if w != h:
+        new_size = max(w, h)
+        side_padding = (h - w) // 2
+        padding = (side_padding, 0, side_padding, 0)
+        img = ImageOps.expand(img, padding)
+        mask = ImageOps.expand(mask, padding)
+        # print(img.size)
+
+        transform = torchvision.transforms.Compose(
+            [
+                torchvision.transforms.Resize([256, 256]),
+                torchvision.transforms.ToTensor(),
+            ]
+        )
+        inp = Variable(transform(img).unsqueeze(0).type("torch.FloatTensor"))
+
+        mask = Variable(
+            transform(mask).expand(3, 256, 256).unsqueeze(0).type("torch.FloatTensor")
+        )
+
+        inp = inp.cuda()
+        mask = mask.cuda()
+    else:
+        transform = torchvision.transforms.Compose(
+            [
+                torchvision.transforms.Resize([256, 256]),
+                torchvision.transforms.ToTensor(),
+            ]
+        )
+        inp = Variable(transform(img).type("torch.FloatTensor"))
+        # print(inp.size())
+        inp = (inp[:3, :, :] * inp[3, :, :]).unsqueeze(0)
+        mask = Variable(transform(mask).type("torch.FloatTensor"))
+        mask = (mask[:3, :, :] * mask[3, :, :]).unsqueeze(0)
+        inp = inp.cuda()
+        mask = mask.cuda()
+    # print(inp.size())
+    # print(mask.size())
+
     decomposer_path = "../logs/separated_decomp_0.01lr_0.1lights/model.t7"
     shader_path = "../logs/separated_shader_0.01/model.t7"
     decomposer = torch.load(decomposer_path)
@@ -33,11 +85,15 @@ if __name__ == "__main__":
     composer = Composer(decomposer, shader).cuda()
     print(composer)
     # pdb.set_trace()
-    inp = Variable(torch.randn(5, 3, 256, 256).cuda())
-    mask = Variable(torch.randn(5, 3, 256, 256).cuda())
+    # inp = Variable(torch.randn(5, 3, 256, 256).cuda())
+    # mask = Variable(torch.randn(5, 3, 256, 256).cuda())
 
     out = composer.forward(inp, mask)
 
     print([i.size() for i in out])
+    imageio.imsave(
+        "recons_composer.png",
+        out.squeeze().detach().numpy().transpose(1, 2, 0).clip(0, 1),
+    )
 
     # pdb.set_trace()
